2999087664aac893329aa2f796e8cdbbe1d76228
seyitaliyaman <seyitaliyaman@gmail.com>
2019-05-06 22:01:46 +0300

    first commit with love

---
+++ src/Client/Main.java
@@ -0,0 +1,100 @@
+package Client;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Main extends Application {
+
+    private DataInputStream fromServer;
+    private DataOutputStream toServer;
+    private TextField textField;
+    private TextArea textArea;
+    private BorderPane paneForTF,mainPane;
+    private Scene scene;
+    private Label label = new Label();
+
+    private Socket socket;
+//C:\Users\HP
+    @Override
+    public void start(Stage primaryStage){
+        label.setText("C:/");
+        paneForTF = new BorderPane();
+        paneForTF.setPadding(new Insets(5,5,5,5));
+        paneForTF.setStyle("-fx-border-color: green");
+        paneForTF.setLeft(label);
+
+        textField = new TextField();
+        textField.setAlignment(Pos.BOTTOM_LEFT);
+
+        paneForTF.setCenter(textField);
+
+        mainPane = new BorderPane();
+        textArea=new TextArea();
+        mainPane.setCenter(new ScrollPane(textArea));
+        mainPane.setTop(paneForTF);
+
+        scene = new Scene(mainPane,450,200);
+        primaryStage.setTitle("CLIENT");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        String cmd = "dir seyit ali dir";
+        String []kelime = null;
+        kelime = cmd.split("dir");
+        for(int i=0; i<kelime.length;  i++){
+            System.out.println(kelime[i]);
+        }
+
+        try {
+            socket = new Socket("localhost",9000);
+            fromServer = new DataInputStream(socket.getInputStream());
+            toServer = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            textArea.appendText(e.getMessage());
+        }
+
+        textField.setOnAction(new ServerSenderHandler());
+    }
+
+    class ServerSenderHandler implements EventHandler<ActionEvent>{
+
+        @Override
+        public void handle(ActionEvent event) {
+
+            String command = textField.getText();
+
+            System.out.println("giden "+command);
+            textField.setText("");
+            try {
+                toServer.writeUTF(command);
+                toServer.flush();
+                String response = fromServer.readUTF();
+                textArea.appendText(response+"\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/Server/Main.java
@@ -0,0 +1,229 @@
+package Server;
+
+import javafx.application.Application;
+
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+
+public class Main extends Application {
+
+
+    private TextArea textArea=new TextArea();
+    private int clientNo = 0;
+    private Scene scene;
+    private ServerSocket serverSocket;
+    private Socket socket;
+    private DataInputStream inputFromClient;
+    private DataOutputStream outputToClient;
+    private String currentDirectory= "C:/Users/HP";
+
+    @Override
+    public void start(Stage primaryStage){
+        scene = new Scene(new ScrollPane(textArea),450,200);
+        primaryStage.setTitle("SERVER");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    serverSocket = new ServerSocket(9000);
+                    textArea.appendText("Server started at "+new Date()+"\n");
+
+                    while (true){
+                        socket = serverSocket.accept();
+                        clientNo++;
+
+                        Platform.runLater(new Runnable() {
+                            @Override
+                            public void run() {
+                                InetAddress inet = socket.getInetAddress();
+                                textArea.appendText("Client "+clientNo+"'s host name is "+inet.getHostName()+"\n");
+                                textArea.appendText("Client "+clientNo+"'s IP Address is "+inet.getHostAddress()+"\n");
+                            }
+                        });
+
+                        new Thread(new HandleClient(socket)).start();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    class HandleClient implements Runnable{
+        private Socket socket;
+
+        HandleClient(Socket socket){
+            this.socket=socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                inputFromClient = new DataInputStream(socket.getInputStream());
+                outputToClient = new DataOutputStream(socket.getOutputStream());
+
+                while (true){
+                    String command = inputFromClient.readUTF();
+                    String cmds[] = command.split(" ");
+                    System.out.println(cmds.length);
+                    //System.out.println(cmds[1]);
+                    String cmdsnd = "";
+                    for(int i=1; i<cmds.length; i++){
+                        cmdsnd+=cmds[i]+" ";
+                    }
+                    System.out.println(cmdsnd);
+
+
+                    switch (cmds[0]){
+                        case "ls":
+                            outputToClient.writeUTF(commandLS(currentDirectory.trim()));
+                            break;
+                        case "cd":
+                            commandCD(cmdsnd);
+                            outputToClient.writeUTF("Directory has changed!");
+                            break;
+                        case "mkdir":
+                            if(commandMKDIR(cmdsnd)){
+                                outputToClient.writeUTF("Directory has created!");
+                            }else{
+                                outputToClient.writeUTF("Directory already exist!");
+                            }
+
+                            break;
+                        case "pwd":
+                            outputToClient.writeUTF(currentDirectory);
+                            break;
+                        case "rm":
+
+                            if(commandRM(cmdsnd)){
+                                outputToClient.writeUTF("Directory has remove!");
+                            }else{
+                                outputToClient.writeUTF("Directory already removed!");
+                            }
+
+                            break;
+                        case "write":
+
+                            break;
+                        case "read":
+
+                            break;
+
+
+                        default:
+
+                            outputToClient.writeUTF("Command doesn't exist!!!");
+                            break;
+                    }
+
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /** ls, cd, mkdir, pwd, rm, write, read **/
+
+
+
+    public String commandLS (String directory){
+        String output = "";
+        List<String> fileNames = new ArrayList<>();
+        try {
+            DirectoryStream<Path> directoryStream = Files.newDirectoryStream(Paths.get(directory));
+            for(Path path : directoryStream){
+                fileNames.add(path.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for(String name: fileNames){
+            output+=name.substring(directory.length()+1)+"\n";
+        }
+        return output;
+    }
+
+    public void commandCD (String directoryName){
+        currentDirectory=directoryName;
+
+    }
+
+    public boolean commandMKDIR (String directoryName){
+
+        //boolean create=false;
+        Path path = Paths.get(currentDirectory.trim()+"\\"+directoryName.trim());
+        if(!Files.exists((path))){
+            try {
+                Files.createDirectories(path);
+                //create=true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return true;
+        }else{
+            return false;
+        }
+
+       /* if(create){
+            return true;
+        }else{
+            return false;
+        }*/
+    }
+
+
+    public boolean commandRM(String fileName){
+        Path path = Paths.get(currentDirectory.trim()+"\\"+fileName.trim());
+        if(Files.exists((path))){
+            try {
+                Files.delete(path);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void commandWRITE(String sourceFileName,String destFileName){
+
+    }
+
+    public void commandREAD(String sourceFileName,String destFileName){
+
+    }
+
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
